659640c35b4e1c91efc773c7ba1392ffa63f5d27
MamankSYQ <abdurrahmansyafiq151123@gmail.com>
2026-09-10 23:39:25 +0700

    Percobaan 2

--- Jobsheet03/Motor.java
+++ Jobsheet03/Motor.java
@@ -1,8 +1,8 @@
 package Jobsheet03;
 public class Motor {
-    public String platNomor;
-    public boolean isMesinOn;
-    public int kecepatan;
+    private String platNomor;
+    private boolean isMesinOn;
+    private int kecepatan;
 
     public void displayStatus(){
         System.out.println("Plat Nomor : " + this.platNomor);
@@ -15,5 +15,41 @@ public class Motor {
 
         System.out.println("Kecepatan : "+this.kecepatan);
         System.out.println("=======================");
+    }
+
+    public String getPlatNomor(){
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor){
+        this.platNomor = platNomor;
+    }
+
+    public boolean isIsMesinOn(){
+        return isMesinOn;
+    }
+
+    public void setIsMesinOn(boolean isMesinOn){
+        this.isMesinOn = isMesinOn;
+    }
+
+    public int getKecepatan(){
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan){
+        if (!this.isMesinOn && kecepatan > 0){
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        }
+        else if (this.isMesinOn && kecepatan > 100){
+            System.out.println("Kecepatan maksimalnya adalah 100");
+            this.kecepatan = 100;
+        }
+        else if (kecepatan < 0){
+            System.out.println("Kecepatan tidak bisa bernilai negatif");
+        }
+        else{
+            this.kecepatan = kecepatan;
+        }
     }
 }
